b2343fb5a9cda74352b228fcaddf8eb2defabcf6
lonelydomino <lonelydomino@icloud.com>
2023-11-14 19:14:42 -0800

    Added new view

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    url('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new')
 ]
